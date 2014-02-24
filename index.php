63d38c864e88971104035276bd5914c8a4975b50
Igor Ferreira <igorcferreira@hotmail.com>
2014-02-23 22:19:26 -0300

    Fix the cedar-support

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php    
+	header('Location: /m/view/index.php');    
+?> 
+<html dir="ltr" lang="pt-br">
+  <head>
+    <title>HTML5 Javascript Tutorial</title>
+    
+    <!-- Meta tags -->
+    <meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <meta content="igorferreira" name="author">
+    <meta content="Página usada para o ensino básico de HTML5 e Javascript" name="description">
+    <meta content="html5, javascript, pogamadores, tutorial" name="keywords">
+    <meta content="BlueGriffon wysiwyg editor" name="generator">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Style sheet imports -->
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css" />
+    
+    <!-- Library imports -->
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js"></script>
+    
+  </head>
+  <body>
+	
+  </body>
+</html>

8bf3bb3fa976cb187ba23edfa4148c8e6dae72ea
Igor Ferreira <igor.monteiro@zapcorp.com.br>
2014-02-24 17:46:16 -0300

    Fix metadatas

--- sendgrid/index.php
+++ sendgrid/index.php
@@ -7,8 +7,8 @@
         <meta content="text/html; charset=utf-8" http-equiv="content-type">
         <meta content="igorferreira" name="author">
         <meta content="Página usada para o ensino básico de HTML5 e Javascript" name="description">
-        <meta content="html5, javascript, pogamadores, tutorial" name="keywords">
-        <meta content="BlueGriffon wysiwyg editor" name="generator">
+        <meta content="html5, javascript, pogamadores, tutorial, sendgrid, pusher" name="keywords">
+        <meta content="Xcode 5.0" name="generator">
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
